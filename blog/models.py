23e77f15ddceef6558021daa8090a2eb5badf5dd
Samane-Gh <samanee.ghasemi>
2023-04-29 10:20:54 +0330

    make a database in model file of blog

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,6 @@
-from django.db import models
-
 # Create your models here.
+from django.db import models
+class post(models.Model):
+    title = models.CharField(max_length=350)
+    content = models.TextField() 
+
