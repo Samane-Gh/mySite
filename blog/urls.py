fc679fa4f4b37c44da929aa050432ef0936532c5
Samane-Gh <samanee.ghasemi>
2023-05-10 10:52:38 +0330

    update view of each post

--- blog/urls.py
+++ blog/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('',blog_view,name='index' ),
     path('single',blog_single,name='single'),
     path('post-<int:pid>',test,name='test'),
+   
     
     
 ] 
--- blog/views.py
+++ blog/views.py
@@ -2,9 +2,8 @@ from django.shortcuts import render,get_object_or_404
 from blog.models import Post
 from django.utils import timezone
 # Create your views here.
-def blog_view(request):
+def blog_view(request,pid=1):
     #posts = Post.objects.filter(status=1)
-    
     posts = Post.objects.filter(published_date__lte=timezone.now())
     contex = {'posts': posts}
     return render(request,'blog/blog-home.html',contex)
@@ -17,17 +16,11 @@ def blog_single(request,pid):
     
 
 def test(request,pid):
-    #post = Post.objects.get(id=pid)
-    post = get_object_or_404(Post,pk= pid)
+    post = Post.objects.get(id=pid)
     contex = {'post': post}
+    post.counted_views +=1
+    post.save()
     return render(request,'test.html',contex)
 
-def blog_post(request,pid):
-    post = Post.objects.get(id=pid)
-    contex ={'post' : post}
-    post.counted_views = 10
-    post.save()
-    
-    return render(request ,'blog/blog-home.html',contex)
     
 
