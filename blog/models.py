5cefc6a4fa7a4adf32f37fb1aa005dac1e64edc6
Samane-Gh <samanee.ghasemi>
2023-05-13 11:03:09 +0330

    add author field

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,8 @@
 # Create your models here.
 from django.db import models
+from django.contrib.auth.models import User
 class Post(models.Model):
-    
+    auther =models.ForeignKey(User, on_delete=models.SET_NULL,null=True)
     title = models.CharField(max_length=350)
     content = models.TextField() 
     counted_views = models.IntegerField()
