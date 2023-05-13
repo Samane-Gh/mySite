8b798d060aed45a4e39e093de64f19b2640a8e9c
Samane-Gh <samanee.ghasemi>
2023-05-13 11:32:30 +0330

    add image field

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 class Post(models.Model):
+    image =models.ImageField(upload_to='blog/',default='blog/defult.jpg')
     auther =models.ForeignKey(User, on_delete=models.SET_NULL,null=True)
     title = models.CharField(max_length=350)
     content = models.TextField() 
