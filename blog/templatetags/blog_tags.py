ae0761136b034500ccda860e000ee687278583a0
Samane-Gh <samanee.ghasemi>
2023-05-16 16:59:24 +0330

    add post category

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,5 +1,6 @@
 from django import template
 from blog.models import Post
+from blog.models import Category
 register = template.Library()
 
 @register.simple_tag(name='totalposts')
@@ -8,7 +9,17 @@ def function():
     posts = Post.objects.filter(status=1)
     return posts
 
-@register.inclusion_tag('popularposts.html')
-def popularposts():
-    posts = Post.objects.filter(status=1).order_by('published_date')[:3]
+@register.inclusion_tag('blog/blog-popular-posts.html')
+def latestposts(arg=3):
+    posts = Post.objects.filter(status=1).order_by('published_date')[:arg]
     return {'posts': posts}
+
+@register.inclusion_tag('blog/blog-post-categories.html')
+def postcategories():
+    posts = Post.objects.filter(status=1)
+    Categories = Category.objects.all()
+    cat_dict = {}
+    for name in Categories:
+        cat_dict[name]=posts.filter(Category=name).count()
+        
+    return {'categories': cat_dict}
