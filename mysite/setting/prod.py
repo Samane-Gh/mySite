dcc49253bf1b4b44c2ce274ed15a43c1435d42af
Samane-Gh <samanee.ghasemi>
2023-06-12 15:09:57 +0330

    last step

--- mysite/setting/prod.py
+++ mysite/setting/prod.py
@@ -2,10 +2,10 @@ from mysite.settings import *
 SECRET_KEY = 'django-insecure-80$q%8&#gpa6&t1qwrgpal8v&aa*#p8-n*^#mi=144r_%9=-(s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["goshopfree.ir", "www.goshopfree.ir"]
-
+#ALLOWED_HOSTS = ["goshopfree.ir", "www.goshopfree.ir"]
+ALLOWED_HOSTS = []
 #site framework
 SITE_ID = 2
 
